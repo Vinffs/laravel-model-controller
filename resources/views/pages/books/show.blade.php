5b1837366e0d4c9719e1ddbd2ecdbca84af5cd3d
Vinffs <vin.demiranda@gmail.com>
2023-12-20 17:32:28 +0100

    added style to show book page

--- resources/views/pages/books/show.blade.php
+++ resources/views/pages/books/show.blade.php
@@ -3,13 +3,17 @@
 @section('title', 'Show Books')
 
 @section('content')
-    <main class="container text-center">
-        <h2>{{ $book->title }}</h2>
+    <main class="container justify-content-center py-4">
         <div class="row">
+            <div class="col-4">
+                <img src="{{ $book->cover_image }}" class="card-img-top my-ratio" alt="{{ $book->title }}">
+            </div>
             <div class="col-8 mx-auto">
-                <div>
-                    <img src="{{ $book->cover_image }}" class="card-img-top my-ratio" alt="{{ $book->title }}">
+                <div class="col-6 d-flex flex-column justify-content-center">
                     <div>
+                        <h5>
+                            <span class="fw-bold">Title: </span>{{ $book->title }}
+                        </h5>
                         <p>
                             <span class="fw-bold">Description:</span> <br>
                             {{ $book->plot }}
