43eb3365e29f03b57dae8022ee2575f99a27841c
Vinffs <vin.demiranda@gmail.com>
2023-12-20 17:27:39 +0100

    added styling to show movies page

--- resources/views/pages/movies/show.blade.php
+++ resources/views/pages/movies/show.blade.php
@@ -3,23 +3,28 @@
 @section('title', 'Show Movies')
 
 @section('content')
-    <main class="container text-center">
-        <h2>{{ $movie->title }}</h2>
-        <div class="row">
-            <div class="col-8 mx-auto">
+    <main class="container">
+        <div class="row justify-content-center py-4">
+            <div class="col-4">
+                <img src="{{ $movie->image }}" class="card-img-top my-ratio" alt="{{ $movie->title }}">
+            </div>
+            <div class="col-6 d-flex flex-column justify-content-center">
                 <div>
-                    <img src="{{ $movie->image }}" class="card-img-top my-ratio" alt="{{ $movie->title }}">
-                    <div>
-                        <h6>
-                            Original Title: {{ $movie->original_title }}
-                        </h6>
-                        <h6>
-                            Vote: {{ $movie->vote }}
-                        </h6>
-                        <h6>
-                            Release Date: {{ $movie->date }}
-                        </h6>
-                    </div>
+                    <h5>
+                        <span class="fw-bold">Title: </span>{{ $movie->title }}
+                    </h5>
+                    <h6>
+                        <span class="fw-bold">Original Title: </span>{{ $movie->original_title }}
+                    </h6>
+                    <h6>
+                        <span class="fw-bold">Vote: </span>{{ $movie->vote }}
+                    </h6>
+                    <h6>
+                        <span class="fw-bold">Language: </span>{{ $movie->language }}
+                    </h6>
+                    <h6>
+                        <span class="fw-bold">Release Date: </span>{{ $movie->date }}
+                    </h6>
                 </div>
             </div>
         </div>
